d71749b5265d4d16dec2aef902f17a3f382a3b59
PrismSpirit <yeolkeut@gmail.com>
2024-08-13 11:20:58 +0900

    feat: 카페상세 View 구현

--- Features/HomeFeature/Sources/Presentation/CafeDetailView.swift
+++ Features/HomeFeature/Sources/Presentation/CafeDetailView.swift
@@ -11,118 +11,159 @@ import WebKit
 struct CafeDetailView: View {
     @State private var showsTagEditSheet: Bool = false
     @State private var showsWebViewSheet: Bool = false
-    var cafe: Cafe
+    @State private var showsRemoveFavoriteAlert: Bool = false
+    
+    @Binding var cafe: Cafe
     
     var body: some View {
-        VStack(spacing: 8) {
-            HStack {
-                Text(cafe.name)
-                    .font(.system(size: 30))
-                    .fontWeight(.bold)
-                
-                Spacer()
-                
-                Image(systemName: "star")
-            }
-            .padding(.horizontal)
-            .padding(.bottom, 4)
-            
-            HStack {
-                Text("주소:")
-                    .font(.system(size: 15))
-                    .fontWeight(.bold)
-                
-                Text(cafe.address)
-                    .font(.system(size: 10))
-                    .fontWeight(.bold)
-                    .foregroundStyle(.gray)
-                
-                Spacer()
-            }
-            .padding(.horizontal)
-            
-            HStack {
-                Text("연락처:")
-                    .font(.system(size: 15))
-                    .fontWeight(.bold)
-                
-                Text(cafe.phone)
-                    .font(.system(size: 10))
-                    .fontWeight(.bold)
-                    .foregroundStyle(.gray)
-                
-                Spacer()
-            }
-            .padding(.horizontal)
-            
-            HStack {
-                Text("태그: ")
-                    .font(.system(size: 15))
-                    .fontWeight(.bold)
-                
-                ForEach(Array(cafe.tags), id: \.self) { tag in
-                    TagView(tag: tag)
+        ZStack {
+            VStack(spacing: 8) {
+                HStack {
+                    Text(cafe.name)
+                        .font(.system(size: 30))
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        showsRemoveFavoriteAlert = true
+                    }) {
+                        Image(systemName: "star.fill")
+                            .font(.title3)
+                            .foregroundStyle(.yellow)
+                    }
+                    
                 }
+                .padding(.horizontal)
+                .padding(.bottom, 4)
                 
-                Spacer()
+                HStack {
+                    Text("주소:")
+                        .font(.system(size: 15))
+                        .fontWeight(.bold)
+                    
+                    Text(cafe.address)
+                        .font(.system(size: 10))
+                        .fontWeight(.bold)
+                        .foregroundStyle(.gray)
+                    
+                    Spacer()
+                }
+                .padding(.horizontal)
+                
+                HStack {
+                    Text("연락처:")
+                        .font(.system(size: 15))
+                        .fontWeight(.bold)
+                    
+                    Text(cafe.phone)
+                        .font(.system(size: 10))
+                        .fontWeight(.bold)
+                        .foregroundStyle(.gray)
+                    
+                    Spacer()
+                }
+                .padding(.horizontal)
+                
+                HStack {
+                    Text("태그:")
+                        .font(.system(size: 15))
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        showsTagEditSheet = true
+                    }) {
+                        Text("태그 수정")
+                            .font(.system(size: 9))
+                            .fontWeight(.semibold)
+                            .padding(.horizontal, 16)
+                            .padding(.vertical, 7)
+                    }
+                    .foregroundStyle(.white)
+                    .background(.black)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+                .padding(.horizontal)
+                
+                HStack {
+                    TagFlowLayout {
+                        ForEach(Array(cafe.tags), id: \.self) { tag in
+                            TagView(tag: tag)
+                                .padding(.horizontal, 4)
+                                .padding(.vertical, 4)
+                        }
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.horizontal)
+                
+                HStack {
+                    Text("위치")
+                        .font(.system(size: 15))
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                }
+                .padding(.horizontal)
+                .padding(.top)
+                
+                GeometryReader { proxy in
+                    MapView(size: proxy.size, cafe: cafe)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                }
+                .frame(maxHeight: 250)
+                .padding(.horizontal)
                 
                 Button(action: {
-                    showsTagEditSheet = true
+                    showsWebViewSheet = true
                 }) {
-                    Text("태그 수정")
-                        .font(.system(size: 9))
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 16)
-                        .padding(.vertical, 7)
+                    Text("상세 보기")
+                        .font(.system(size: 15))
+                        .fontWeight(.bold)
+                        .padding()
+                        .frame(maxWidth: .infinity)
                 }
                 .foregroundStyle(.white)
                 .background(.black)
                 .clipShape(RoundedRectangle(cornerRadius: 10))
-            }
-            .padding(.horizontal)
-            .padding(.bottom)
-            
-            HStack {
-                Text("위치")
-                    .font(.system(size: 15))
-                    .fontWeight(.bold)
+                .padding()
                 
                 Spacer()
             }
             .padding(.horizontal)
-            .padding(.top)
-            
-            GeometryReader { proxy in
-                MapView(size: proxy.size, position: cafe.coord)
-                    .clipShape(RoundedRectangle(cornerRadius: 15))
-            }
-            .frame(maxHeight: 250)
-            .padding(.horizontal)
-            
-            Button(action: {
-                showsWebViewSheet = true
+            .sheet(isPresented: $showsWebViewSheet, onDismiss: {
+                showsWebViewSheet = false
             }) {
-                Text("상세 보기")
-                    .font(.system(size: 15))
-                    .fontWeight(.bold)
-                    .padding()
-                    .frame(maxWidth: .infinity)
+                WebView(url: URL(string: cafe.placeURL)!)
             }
-            .foregroundStyle(.white)
-            .background(.black)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .padding()
+            .sheet(isPresented: $showsTagEditSheet, onDismiss: { showsTagEditSheet = false }) {
+                TagEditView(selectedTags: $cafe.tags)
+            }
             
-            Spacer()
-        }
-        .padding(.horizontal)
-        .sheet(isPresented: $showsWebViewSheet, onDismiss: {
-            showsWebViewSheet = false
-        }) {
-            WebView(url: URL(string: cafe.placeURL)!)
-        }
-        .sheet(isPresented: $showsTagEditSheet, onDismiss: { showsTagEditSheet = false }) {
-            TagEditView()
+            ZStack {
+                Color.gray
+                    .opacity(showsRemoveFavoriteAlert ? 0.6 : 0.0)
+                
+                FindaAlertView(
+                    isPresented: $showsRemoveFavoriteAlert,
+                    title: nil,
+                    description: "\(cafe.name) 을 즐겨찾기에서 삭제할까요?",
+                    cancelText: "아니요",
+                    confirmText: "네",
+                    cancelAction: {
+                        showsRemoveFavoriteAlert = false
+                    },
+                    confirmAction: {
+                        print("remove")
+                        showsRemoveFavoriteAlert = false
+                    }
+                )
+            }
+            .ignoresSafeArea()
+            .opacity(showsRemoveFavoriteAlert ? 1.0 : 0.0)
         }
     }
 }
