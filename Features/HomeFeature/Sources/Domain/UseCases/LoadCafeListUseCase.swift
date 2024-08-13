60c13c530bc516aeb189773e27a5508a35ccdbf5
PrismSpirit <yeolkeut@gmail.com>
2024-08-13 11:16:05 +0900

    test: Tag 구현 변경에 따른 mock 수정

--- Features/HomeFeature/Sources/Domain/UseCases/LoadCafeListUseCase.swift
+++ Features/HomeFeature/Sources/Domain/UseCases/LoadCafeListUseCase.swift
@@ -30,8 +30,8 @@ public final class LoadCafeListUseCaseMock: LoadCafeListUseCase {
     
     public func execute() async throws -> [Cafe] {
         return [
-            Cafe(id: "1234", name: "스타벅스", address: "경기도 안양시 동안구 시민대로 222", phone: "031-123-1234", coord: Coord(longitude: 126.9533556, latitude: 37.3897), placeURL: "https://naver.com", tags: [Tag(name: "조용함", category: .mood), Tag(name: "혼잡", category: .congestion)]),
-            Cafe(id: "2345", name: "이디야", address: "경기도 안양시 동안구 시민대로 223", phone: "031-123-1234", coord: Coord(longitude: 126.8468194, latitude: 37.29851944), placeURL: "https://google.com", tags: [Tag(name: "시끄러움", category: .mood), Tag(name: "넓음", category: .cafeSize), Tag(name: "상쾌함", category: nil)])
+            Cafe(id: "1234", name: "스타벅스", address: "경기도 안양시 동안구 시민대로 222", phone: "031-123-1234", coord: Coord(longitude: 126.9533556, latitude: 37.3897), placeURL: "https://naver.com", tags: [Tag(name: "조용함", category: .mood), Tag(name: "혼잡", category: .congestion), Tag(name: "asdf", category: .custom), Tag(name: "asdf11", category: .custom), Tag(name: "qwer", category: .custom), Tag(name: "zxcv131", category: .custom), Tag(name: "poiu4423", category: .custom)]),
+            Cafe(id: "2345", name: "이디야", address: "경기도 안양시 동안구 시민대로 223", phone: "031-123-1234", coord: Coord(longitude: 126.8468194, latitude: 37.29851944), placeURL: "https://google.com", tags: [Tag(name: "시끄러움", category: .mood), Tag(name: "넓음", category: .cafeSize), Tag(name: "상쾌함", category: .custom), Tag(name: "김", category: .custom)])
         ]
     }
 }
